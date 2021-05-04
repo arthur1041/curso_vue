ac8701fc74def40e7074313bee9d476ed08d9cb3
Arthur Cruz <arthur.silva1041@gmail.com>
2021-05-03 22:36:46 -0300

    simplecommit

--- desafios/desafio-02/app.js
+++ desafios/desafio-02/app.js
@@ -1,6 +1,18 @@
 new Vue({
     el: '#desafio',
     data: {
-        valor: ''
+        valor: '',
+        valor2: ''
+    },
+    methods: {
+        alertar: function(){
+            alert("alerta!!!");
+        },
+        armazenarValor: function(event){
+            this.valor = event.target.value;
+        },
+        armazenarValor2: function(event){
+            this.valor2 = event.target.value;
+        }
     }
 })
